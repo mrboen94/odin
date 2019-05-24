f1f30a660e87ecda37aab9bcd161a63b8efdd674
Mathias Bøe <mrboen94@gmail.com>
2019-05-24 21:31:25 +0200

    small changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,4 @@
 // Import the Button component from this folder and send it down to ./components/index.js
-import Button from './Button';
+import Card from './Card';
 
-export default Button;
+export default Card;
